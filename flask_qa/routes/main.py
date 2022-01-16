918d22521fe6f5433a6534dee818fb79337da981
batman <dharg57@gmail.com>
2022-01-16 21:41:02 +0530

    hi

--- flask_qa/routes/main.py
+++ flask_qa/routes/main.py
@@ -78,7 +78,7 @@ def apiToPostStats():
 @main.route('/api/coord', methods=['GET'])
 def apiToGetCoords():
     if request.method == 'GET':
-        stats = Stats.query.all()
+        stats = Stats.query.filter().all()
 
         return {stats}, 200
 
